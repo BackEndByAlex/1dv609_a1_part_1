7be57b9900914b3e0f9243a0141fea28078ab752
Alexandru Cristian Antonescu <alexsept100@gmail.com>
2025-11-18 13:50:01 +0100

    add BugisPasswordAlwaysSame test

--- js/practice/practice_write_run_tests/tests/password.test.js
+++ js/practice/practice_write_run_tests/tests/password.test.js
@@ -3,13 +3,13 @@ import { assert, describe, it, expect } from "vitest"
 
 // import { Password } from "../src/BugDoesNotHash"
 // import { Password } from "../src/BugDoesNotTrim"
-// import { Password } from '../src/BugisPasswordAlwaysSame'
+// import { Password } from "../src/BugisPasswordAlwaysSame"
 // import { Password } from '../src/BugMissingNumberCheck'
 // import { Password } from '../src/BugMissingPasswordCheck'
 // import { Password } from '../src/BugNeverContainsNumbers'
 // import { Password } from '../src/BugToShortPassword'
 // import { Password } from '../src/BugVeryShort'
-// import { Password } from '../src/BugWrongHashingAlgorithm'
+// import { Password } from "../src/BugWrongHashingAlgorithm"
 // import { Password } from '../src/BugWrongMessage'
 import { Password } from "../src/Correct"
 
@@ -37,5 +37,19 @@ describe("BugDoesNotTrim", () => {
     const hashpasswordTwo = passwordTwo.getPasswordHash()
 
     expect(hashpasswordOne).toBe(hashpasswordTwo)
+  })
+})
+
+describe("BugisPasswordAlwaysSame", () => {
+  it("should check if passwords are the same", () => {
+    const inputpassword = "password 12345"
+    const inputpasswordTwo = "password12345"
+
+    const password = new Password(inputpassword)
+    const passwordTwo = new Password(inputpasswordTwo)
+
+    const expectpassword = password.isPasswordSame(passwordTwo)
+
+    expect(expectpassword).toBe(false)
   })
 })
