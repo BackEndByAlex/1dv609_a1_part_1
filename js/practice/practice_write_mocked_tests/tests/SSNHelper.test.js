ca39d46afd47863493b290e7e1c8898473fb0600
Alexandru Cristian Antonescu <alexsept100@gmail.com>
2025-11-20 15:09:41 +0100

    add BuggySSNHelperAllowMonth0 tests

--- js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
+++ js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
@@ -1,15 +1,28 @@
 import { describe, expect, it } from "vitest"
 
-import { SSNHelper } from "../src/bugs/BuggySSNHelperAllowDayUpTo30"
+// import { SSNHelper } from "../src/bugs/BuggySSNHelperAllowDayUpTo30"
+// import { SSNHelper } from "../src/bugs/BuggySSNHelperAllowMonth0"
+import { SSNHelper } from "../src/bugs/BuggySSNHelperIncorrectFormat"
 // import { SSNHelper } from "../src/correct/SSNHelper"
 
 describe("Allow upp to 30 days", () => {
-  it("isValid should return true for valid date on the 31st (t.ex Janary)", () => {
+  it("isValidDay should return true for valid date on the 31st (t.ex Janary)", () => {
     const validSSNcheck31st = "31"
 
     const helper = new SSNHelper()
     const validDate = helper.isValidDay(validSSNcheck31st)
 
     expect(validDate).toBe(true)
+  })
+})
+
+describe("Allow month 0", () => {
+  it("isValidMounth should return false for valid mounth 0", () => {
+    const validSSNcheck0Mounth = "0"
+
+    const helper = new SSNHelper()
+    const validMounth = helper.isValidMonth(validSSNcheck0Mounth)
+
+    expect(validMounth).toBe(false)
   })
 })
