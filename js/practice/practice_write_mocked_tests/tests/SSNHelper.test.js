7c77a6216ea1dccf1ac55ba007487ede3d08068a
Alexandru Cristian Antonescu <alexsept100@gmail.com>
2025-11-20 15:14:50 +0100

    add BuggySSNHelperIncorrectFormat test

--- js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
+++ js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
@@ -2,8 +2,8 @@ import { describe, expect, it } from "vitest"
 
 // import { SSNHelper } from "../src/bugs/BuggySSNHelperAllowDayUpTo30"
 // import { SSNHelper } from "../src/bugs/BuggySSNHelperAllowMonth0"
-import { SSNHelper } from "../src/bugs/BuggySSNHelperIncorrectFormat"
-// import { SSNHelper } from "../src/correct/SSNHelper"
+// import { SSNHelper } from "../src/bugs/BuggySSNHelperIncorrectFormat"
+import { SSNHelper } from "../src/correct/SSNHelper"
 
 describe("Allow upp to 30 days", () => {
   it("isValidDay should return true for valid date on the 31st (t.ex Janary)", () => {
@@ -24,5 +24,16 @@ describe("Allow month 0", () => {
     const validMounth = helper.isValidMonth(validSSNcheck0Mounth)
 
     expect(validMounth).toBe(false)
+  })
+})
+
+describe("Check the string format", () => {
+  it("isCorrectFormat should return false for wronge format", () => {
+    const wrongeFormat = "232212-13212"
+
+    const helper = new SSNHelper()
+    const validFormat = helper.isCorrectFormat(wrongeFormat)
+
+    expect(validFormat).toBe(false)
   })
 })
