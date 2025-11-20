5cef53973800adbedbc7b77ee0976adb93ef006d
Alexandru Cristian Antonescu <alexsept100@gmail.com>
2025-11-20 15:03:34 +0100

    add BuggySSNHelperAllowDayUpTo30 test

--- js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
+++ js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
@@ -1,8 +1,15 @@
-import { describe, expect, vi } from "vitest"
+import { describe, expect, it } from "vitest"
 
 import { SSNHelper } from "../src/bugs/BuggySSNHelperAllowDayUpTo30"
 // import { SSNHelper } from "../src/correct/SSNHelper"
 
 describe("Allow upp to 30 days", () => {
-  it('')
+  it("isValid should return true for valid date on the 31st (t.ex Janary)", () => {
+    const validSSNcheck31st = "31"
+
+    const helper = new SSNHelper()
+    const validDate = helper.isValidDay(validSSNcheck31st)
+
+    expect(validDate).toBe(true)
+  })
 })
