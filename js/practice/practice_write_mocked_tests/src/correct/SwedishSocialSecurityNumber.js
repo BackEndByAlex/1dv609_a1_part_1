f887478f2700010b739b172201248ee6bfc06f73
Alexandru Cristian Antonescu <alexsept100@gmail.com>
2025-11-25 14:13:30 +0100

    update the correct SSSNumber class, and add BuggySwedishSocialSecurityNumberNoLenCheck test

--- js/practice/practice_write_mocked_tests/src/correct/SwedishSocialSecurityNumber.js
+++ js/practice/practice_write_mocked_tests/src/correct/SwedishSocialSecurityNumber.js
@@ -1,51 +1,48 @@
-
-
-
 //Java Version 1 Correct Password
 class SwedishSocialSecurityNumber {
-    #helper;
-    #ssn;
-    
-    constructor(stringInput, helper) {
-        this.#helper = helper;
+  #helper
+  #ssn
 
-        const trimmedSS = stringInput.trim(); 
+  constructor(stringInput, helper) {
+    this.#helper = helper
 
-        if (helper.isNotCorrectLength(trimmedSS)) {
-            throw new Error("To short, must be 11 characters");
-        }
-        if (helper.isCorrectFormat(trimmedSS) === false) {
-            throw new Error("Incorrect format, must be: YYMMDD-XXXX");
-        }
+    const trimmedSS = stringInput.trim()
 
-        this.#ssn = trimmedSS;
-
-        if (helper.isValidMonth(this.getMonth()) === false) {
-            throw new Error("Invalid month in SSN");
-        }
-        if (helper.isValidDay(this.getDay()) === false) {
-            throw new Error("Invalid month in SSN");
-        }
-        if (helper.luhnisCorrect(this.#ssn) === false) {
-            throw new Error("Invalid SSN according to Luhn's algorithm");
-        }
+    if (helper.isCorrectLength(trimmedSS) === false) {
+      throw new Error("To short, must be 11 characters")
+    }
+    if (helper.isCorrectFormat(trimmedSS) === false) {
+      throw new Error("Incorrect format, must be: YYMMDD-XXXX")
     }
 
-    getYear() {
-        return this.#ssn.substring(0, 2); //YYMMDD-XXXX
-    }
+    this.#ssn = trimmedSS
 
-    getMonth() {
-        return this.#ssn.substring(2, 4); //YYMMDD-XXXX
+    if (helper.isValidMonth(this.getMonth()) === false) {
+      throw new Error("Invalid month in SSN")
     }
+    if (helper.isValidDay(this.getDay()) === false) {
+      throw new Error("Invalid month in SSN")
+    }
+    if (helper.luhnisCorrect(this.#ssn) === false) {
+      throw new Error("Invalid SSN according to Luhn's algorithm")
+    }
+  }
 
-    getDay() {
-        return this.#ssn.substring(4, 6); //YYMMDD-XXXX  0123456-8901
-    }
+  getYear() {
+    return this.#ssn.substring(0, 2) //YYMMDD-XXXX
+  }
 
-    getSerialNumber() {
-        return this.#ssn.substring(7, 11); //YYMMDD-XXXX
-    }
+  getMonth() {
+    return this.#ssn.substring(2, 4) //YYMMDD-XXXX
+  }
+
+  getDay() {
+    return this.#ssn.substring(4, 6) //YYMMDD-XXXX  0123456-8901
+  }
+
+  getSerialNumber() {
+    return this.#ssn.substring(7, 11) //YYMMDD-XXXX
+  }
 }
 
 // Export Password as main export for backward compatibility, and Helper as a property
--- js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
+++ js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
@@ -1,14 +1,31 @@
-import { SwedishSocialSecurityNumber } from '../src/correct/SwedishSocialSecurityNumber'; 
+import { beforeEach, describe, expect, vi, it } from "vitest"
 
+// import { SwedishSocialSecurityNumber } from "../src/bugs/BuggySwedishSocialSecurityNumberNoLenCheck"
+// import { SwedishSocialSecurityNumber } from "../src/bugs/BuggySwedishSocialSecurityNumberNoTrim"
+// import { SwedishSocialSecurityNumber } from "../src/bugs/BuggySwedishSocialSecutityNumberNoLuhn"
+// import { SwedishSocialSecurityNumber } from "../src/bugs/BuggySwedishSocialSecutityNumberWrongYear"
+import { SwedishSocialSecurityNumber } from "../src/correct/SwedishSocialSecurityNumber"
 
+vi.mock("../src/correct/SSNHelper")
 
-//NOTE THESE TESTS SHOULD NOT BE DEPENDENT ON SSNHelper BUT USE MOCKING
-describe('SwedishSocialSecurityNumber Tests', () => {
-    //put constants here to increase readability
+describe("SwedishSocialSecurityNumber Controller and Validation", () => {
+  let helper
 
-    test('replace this test with one of your own', () => {
-        expect(true).toBe(true);
-    });
+  beforeEach(() => {
+    helper = {
+      isCorrectLength: vi.fn(),
+      isCorrectFormat: vi.fn(),
+      isValidMonth: vi.fn(),
+      isValidDay: vi.fn(),
+      luhnisCorrect: vi.fn(),
+    }
+  })
 
-    //Add your tests here
-});
+  it("Constructor Should Throw Error if the lenght if wrong", () => {
+    helper.isCorrectLength.mockReturnValue(false)
+
+    expect(() => {
+      new SwedishSocialSecurityNumber("821221-1165", helper)
+    }).toThrow("To short, must be 11 characters")
+  })
+})
